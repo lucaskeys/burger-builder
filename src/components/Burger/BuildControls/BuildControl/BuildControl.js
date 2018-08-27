a58120b710764c0f4a4dafc2d44ecfc190e54f3a
lk <lucaskeys@gmail.com>
2018-08-26 18:00:43 -0700

    Add control component with labels and control buttons

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './BuildControl.css';
+const buildControl = (props) => (
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button className={classes.Less}>Remove</button>
+    <button className={classes.More}>Add More</button>
+  </div>
+);
+
+export default buildControl;

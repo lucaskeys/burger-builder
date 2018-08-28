c6ac02e9c95df93c97cf962276bec604a2371438
lk <lucaskeys@gmail.com>
2018-08-27 23:31:41 -0700

    Add styling to close modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -73,8 +73,12 @@ class BurgerBuilder extends Component {
     this.updatedPurchaseState(updatedIngredients)
   }
 
-  purchaseHandler() {
+  purchaseHandler = () => {
     this.setState({purchasing: true})
+  }
+
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false})
   }
 
   render() {
@@ -89,7 +93,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients} />

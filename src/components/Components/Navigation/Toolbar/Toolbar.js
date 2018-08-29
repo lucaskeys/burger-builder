2375b78f788dbbae5c5cf93d988e0b226ed4e590
lk <lucaskeys@gmail.com>
2018-08-28 22:16:34 -0700

    Add base nav component

---
+++ src/components/Components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const toolbar = (props) => {
+  <header>
+    <div>Menu</div>
+    <div>Logo</div>
+    <nav>
+      <ul></ul>
+    </nav>
+  </header>
+}
+
+
+export defautl toolbar;

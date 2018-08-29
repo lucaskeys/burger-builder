25f75ae2da460f0d200831135ddd56a124fedbed
lk <lucaskeys@gmail.com>
2018-08-28 21:58:23 -0700

    Add button functionality to cancel or continue purchase

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
-import Button from '../../UI/Button/Button.js'
+import Button from '../../UI/Button/Button.js';
+
+
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
     return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
@@ -12,9 +14,10 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
-      <Button btnType="Danger">CANCEL</Button>
-      <button>CONTINUE</button>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
     </Aux>
   )
 }
